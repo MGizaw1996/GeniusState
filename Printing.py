1648fd4df0be38e425b7aa8d694ce824b8e88a59
Martha Gizaw <34733400+MGizaw1996@users.noreply.github.com>
2021-02-08 14:40:49 -0500

    Add files via upload

---
+++ Printing.py
@@ -0,0 +1,84 @@
+# Martha Gizaw
+# February 8, 2021
+# File: Printing.py
+# Project: The Genius State
+# Step 1: Hello World! Let’s create print statements of anything we want the computer to say. Also include loops and functions for printing larger statements.
+
+# Print a single statement.
+print("Hello World!\n")
+# or
+print("Hello World!")
+print()
+
+# Print two statements. \n = new line
+print("Hello World! May I take your order?\n")
+
+# Print two statements stacked on each other.
+print("Hello World! \nMay I take your order?\n")
+
+# Create a for loop to repeat a statement 5 times.
+x = "'Hello World!' says x."
+
+for i in range(5):
+    print(x)
+print()
+# OR
+y = 5
+print(y)
+for i in range(y):
+    print(x)
+print()
+
+# =====================================================
+
+print(3)
+print(3+3)
+
+x = 10
+print(3+x)
+print(x-3)
+print(3-x)
+print(3*x)
+print(3/x)
+print(3//x)
+print(x**3)
+print()
+
+# =======================================================
+# Project Ideas
+# + Math Quiz
+# + Spelling Quiz
+# + Number guessing game
+# + Build a calculator
+
+# ========================================================
+
+# If-else statements
+
+# ========================================================
+
+# Create the math quiz
+# Create a spelling quiz
+# Create a short story
+
+# ========================================================
+
+# Convert x, the integer, to a string value using str()
+x = 0
+while (x < 10):
+    print("Hello world!")
+    x += 1
+    if (x == 10):
+        print("BYE!")
+        print()
+
+while (x < 20):
+    print(str(x) + ". Hello world!")
+    x += 1
+    if (x == 20):
+        print(str(x),'. BYE!')
+        print()
+
+# ========================================================
+# Create a BIG Python programming Test!
+# Do the same for 4 other languages: MATLAB, R
